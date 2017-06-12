e03ea410533079e239e8208c42bbe2fcd5ceb382
hardas yang <hardas.yang@gmail.com>
2017-06-12 22:41:52 +0800

    redesign to use word rank matrix

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"bufio"
-	"encoding/gob"
+	// "encoding/gob"
 	"fmt"
 	"io"
-	"math"
+	// "math"
 	"os"
-	"sort"
+	// "sort"
 	"strconv"
 	"strings"
 	"unicode"
@@ -22,12 +22,33 @@ import (
 
 // Map "word" as key, array of
 // map[string][p][s]int, [p] is paragraph, 0->n; [s] is sentence, 1->n, and 0 is [p]'s class
-var words_map map[string][][2]int
+// var words_map map[string][][2]int
+var words_map map[string]Word
 
-// {
-// 	"good": {{0, 1}, {1, 1}, {2, 2}}, <- word of "good" appears in #0 paragraph's #1 sentence, and #1p's #1s, and #2p's #2s;
-// 	"bad": {{0, 2}, {1, 3}, {1, 4}}, <- word of "bad" appears in #0p's #2s, and #1p's #3s, and #1p's #4s
-// }
+type Word struct {
+	// Name       string
+	Appearance [][2]int
+	// {
+	// 	"good": {{0, 1}, {1, 1}, {2, 2}}, <- word of "good" appears in #0 paragraph's #1 sentence, and #1p's #1s, and #2p's #2s;
+	// 	"bad": {{0, 2}, {1, 3}, {1, 4}}, <- word of "bad" appears in #0p's #2s, and #1p's #3s, and #1p's #4s
+	// }
+	WordRankOnEmergeTimes [5]float64
+	WordRankOnEmergeRatio [5]float64
+	// Frequency      int
+	// RateOfFreq     float64
+	// Class_Average  float64
+	// Class_Variance float64
+	// Weight         float64
+}
+
+var trainingParagraph_array []Paragraph
+var testingParagraph_array []Paragraph
+
+type Paragraph struct {
+	Rank           int
+	Sentences      []string
+	RankPrediction int
+}
 
 var trainingSentences [][]string
 
@@ -41,15 +62,7 @@ var trainingSentences [][]string
 
 var testingSentences [][]string
 
-type Word struct {
-	Name           string
-	Frequency      int
-	RateOfFreq     float32
-	Class_Average  float32
-	Class_Variance float32
-	Weight         float32
-}
-
+/*
 type Words []Word
 
 func (sort Words) Len() int {
@@ -64,6 +77,7 @@ func (sorter Words) Swap(i, j int) {
 	sorter[i], sorter[j] = sorter[j], sorter[i]
 }
 
+
 var words_post_analysis_map map[string]Word
 
 type GobShell struct {
@@ -72,6 +86,8 @@ type GobShell struct {
 }
 
 var gobFileName string = "trainingdata.gob"
+*/
+
 var testingFileName string = "dev.txt"
 var verificationResultFileName string = "dev_result.txt"
 
@@ -89,11 +105,8 @@ var debug bool = false
 func main() {
 	fmt.Println("Program Start...")
 
-	// base data initialization
-	InitializeBaseData()
-
-	// analysis words requency
-	WordsAnalysis()
+	// train data process
+	Training()
 
 	// result verification
 	Verification()
@@ -105,12 +118,12 @@ func main() {
 
 func Score() {
 	j := 0
-	for _, s := range output {
-		if s.CalculatedClass == s.ActualClass {
+	for _, s := range testingParagraph_array {
+		if s.Rank == s.RankPrediction {
 			j++
 		}
 	}
-	fmt.Println("match:", float64(j)/float64(len(output)))
+	fmt.Println("match:", float64(j)/float64(len(testingParagraph_array)))
 }
 
 func Verification() {
@@ -119,9 +132,6 @@ func Verification() {
 	defer testingFile.Close()
 
 	notALetter := func(char rune) bool { return !unicode.IsLetter(char) }
-	// var output []string
-
-	var verificationResultClass []float64
 
 	br := bufio.NewReader(testingFile)
 	for i := 0; ; i++ {
@@ -133,180 +143,36 @@ func Verification() {
 		para := string(line)
 
 		t, _ := strconv.Atoi(para[:strings.IndexFunc(para, unicode.IsSpace)])
-		actualClass := float64(t)
+		paragraph := Paragraph{Rank: int(t), Sentences: []string{para}}
 
-		// class := strings.IndexFunc(para, unicode.IsSpace)
-		// // fmt.Println("class: ", para[:class])
-		// // e.g. class:  3
-
-		var class float32 = 0
-		var match int = 0
-
+		var ratioOfParagraph [5]float64
+		// sum each word's ratio as paragraph's ratio
 		for _, word := range strings.FieldsFunc(para, notALetter) {
 			word = strings.ToLower(word)
 
-			if word_post_analysis, ok := words_post_analysis_map[word]; ok {
-				class += word_post_analysis.Class_Average
-				match++
+			if wordStatisic, ok := words_map[word]; ok {
+				for i := 0; i < len(ratioOfParagraph); i++ {
+					ratioOfParagraph[i] += wordStatisic.WordRankOnEmergeRatio[i]
+				}
 			}
 		}
-		// output = append(output, strconv.FormatFloat(float64(class)/float64(match), 'f', 2, 32)+" "+para)
-		verificationResultClass = append(verificationResultClass, float64(class)/float64(match))
-		output = append(output, testingFileOutput{EstimatedWeight: float64(class) / float64(match), ActualClass: actualClass, Content: para})
+
+		// found the max rank posibility
+		rankPredict := 0
+		ratioPredict := 0.0
+		for i, v := range ratioOfParagraph {
+			if v > ratioPredict {
+				ratioPredict = v
+				rankPredict = i
+			}
+		}
+		paragraph.RankPrediction = rankPredict
+		testingParagraph_array = append(testingParagraph_array, paragraph)
 	}
 
-	// analysis training data's class distribution
-	var trainingDataClassSatatic [5]int
-	for i, _ := range trainingSentences {
-		j, _ := strconv.Atoi(trainingSentences[i][0])
-		switch j {
-		case 0:
-			trainingDataClassSatatic[0]++
-		case 1:
-			trainingDataClassSatatic[1]++
-		case 2:
-			trainingDataClassSatatic[2]++
-		case 3:
-			trainingDataClassSatatic[3]++
-		case 4:
-			trainingDataClassSatatic[4]++
-		}
-	}
-	fmt.Println(trainingDataClassSatatic)
-	// e.g. [1082 2228 1621 2328 1285]
-	var trainingDataClassPercentage [5]float64
-	for i, _ := range trainingDataClassSatatic {
-		trainingDataClassPercentage[i] = float64(trainingDataClassSatatic[i]) / float64(len(trainingSentences))
-	}
-	fmt.Println(trainingDataClassPercentage)
-	// e.g. [0.1266385767790262 0.2607677902621723 0.1897237827715356 0.27247191011235955 0.15039794007490637]
-	// calculate class thresholds
-	sort.Float64s(verificationResultClass)
-	// fmt.Println(verificationResultClass)
-	var classThreshods [5]float64
-	var p float64
-	for i, _ := range trainingDataClassPercentage {
-		p += trainingDataClassPercentage[i]
-		classThreshods[i] = verificationResultClass[int(float64(len(verificationResultClass))*p)]
-	}
-	fmt.Println(classThreshods)
-	// e.g. [1.851025644938151 2.0055873870849608 2.0936654743395353 2.272305443173363 2.8736173629760744]
-	for i, _ := range output {
-		switch {
-		case output[i].EstimatedWeight < classThreshods[0]:
-			output[i].CalculatedClass = 0
-		case output[i].EstimatedWeight < classThreshods[1]:
-			output[i].CalculatedClass = 1
-		case output[i].EstimatedWeight < classThreshods[2]:
-			output[i].CalculatedClass = 2
-		case output[i].EstimatedWeight < classThreshods[3]:
-			output[i].CalculatedClass = 3
-		default:
-			output[i].CalculatedClass = 4
-
-		}
-	}
-	// for _, s := range output {
-	// 	fmt.Println(s.CalculatedClass, s.ActualClass, s.Content)
+	// for _, v := range testingParagraph_array {
+	// 	fmt.Println(v.Rank, v.RankPrediction)
 	// }
-}
-
-func WordsAnalysis() {
-	var totalWordsCount int = 0
-	for _, value := range words_map {
-		totalWordsCount = totalWordsCount + len(value)
-
-	}
-	fmt.Println("total words:", totalWordsCount)
-
-	var overallAverageClass float32 = 0
-	for i, _ := range trainingSentences {
-		class, _ := strconv.Atoi(trainingSentences[i][0])
-		overallAverageClass = overallAverageClass + float32(class)
-	}
-	overallAverageClass = overallAverageClass / float32(len(trainingSentences))
-	fmt.Println("overall average class:", overallAverageClass)
-
-	var words Words
-	words_post_analysis_map = make(map[string]Word)
-
-	// update how many times a word appears in whole training data set, and its percentage
-	for key, value := range words_map {
-		words = append(words, Word{Name: key, Frequency: len(value), RateOfFreq: float32(len(value)) / float32(totalWordsCount)})
-	}
-
-	// update the average of paragraph's classes containing the word
-	for i, word := range words {
-		// get [][2]int of each word
-		v, _ := words_map[word.Name]
-
-		var sum int = 0
-		for _, p := range v {
-			class, _ := strconv.Atoi(trainingSentences[p[0]][0])
-			sum = sum + class
-		}
-		words[i].Class_Average = float32(sum) / float32(len(v))
-		// update variance of each word
-		words[i].Class_Variance = float32(math.Pow(float64(words[i].Class_Average-overallAverageClass), 2))
-
-		words[i].Weight = words[i].Class_Variance * words[i].RateOfFreq
-
-		// write to post analysis word map
-		words_post_analysis_map[words[i].Name] = words[i]
-	}
-
-	sort.Sort(sort.Reverse(words))
-	// for i, word := range words {
-	// 	fmt.Printf("#.%d: %s , %d , %.9f , %.9f , %.9f , %.9f\n", i, word.Name, word.Frequency, word.RateOfFreq, word.Class_Average, word.Class_Variance, word.Weight)
-	// }
-}
-
-func InitializeBaseData() {
-	// if train data stuct file exists, load data directly; else read original train txt file and save to data struct file
-	_, err := os.Stat(gobFileName)
-	if err != nil && os.IsNotExist(err) {
-		// read practice data from train.txt
-		ReadPracticeData()
-
-		// persistence of train data as structure
-		gobShellSave := GobShell{Words_map: words_map, Sentences: trainingSentences}
-		SaveTrainingDataStruct(gobFileName, gobShellSave)
-		fmt.Println("pre-loaded file doesn't exist")
-	} else {
-		// load train data structure from file
-		gobShellload := new(GobShell)
-		LoadTrainDataStruct(gobFileName, gobShellload)
-		words_map = gobShellload.Words_map
-		trainingSentences = gobShellload.Sentences
-		fmt.Println("pre-loaded file exists")
-	}
-
-	// fmt.Println(words_mapload)
-	if debug {
-		fmt.Println(trainingSentences)
-
-		for key, value := range words_map {
-			fmt.Printf("%s : %d\n", key, value)
-		}
-	}
-}
-
-func LoadTrainDataStruct(file string, v interface{}) {
-	loadFile, err := os.Open(file)
-	Check(err, "cannot load saved file")
-	defer loadFile.Close()
-	decoder := gob.NewDecoder(loadFile)
-	err = decoder.Decode(v)
-	Check(err, "error in decoding from saved file")
-}
-
-func SaveTrainingDataStruct(file string, v interface{}) {
-	saveFile, err := os.Create(file)
-	Check(err, "save file cannot be created")
-	defer saveFile.Close()
-	encoder := gob.NewEncoder(saveFile)
-	err = encoder.Encode(v)
-	Check(err, "error in encoding to save file")
 }
 
 func Check(err error, log string) {
@@ -316,14 +182,14 @@ func Check(err error, log string) {
 	}
 }
 
-func ReadPracticeData() {
+func Training() {
 	// practiceFile, err := ioutil.ReadFile("train.txt")
 	practiceFile, err := os.Open("train.full.txt")
 	Check(err, "practice file cannot be loaded")
 	defer practiceFile.Close()
 
 	notALetter := func(char rune) bool { return !unicode.IsLetter(char) }
-	words_map = make(map[string][][2]int)
+	words_map = make(map[string]Word)
 
 	br := bufio.NewReader(practiceFile)
 	for i := 0; ; i++ {
@@ -335,34 +201,61 @@ func ReadPracticeData() {
 		//read a paragragh
 		para := string(line)
 		// fmt.Println("Para ", i, ": ", para)
+		class_position := strings.IndexFunc(para, unicode.IsSpace)
 
 		//read the paragragh's class
-		class := strings.IndexFunc(para, unicode.IsSpace)
+		class, _ := strconv.Atoi(para[:class_position])
+		paragraph := Paragraph{Rank: class}
 		// fmt.Println("class: ", para[:class])
-		// e.g. class:  3
-		currentSentence := []string{para[:class]}
+		// e.g. {0 [0 Her fans walked out muttering words like `` horrible '' and `` terrible   '' but had so much fun dissing the film that they did n't mind the ticket cost .]}
+		// e.g. {1 [1 In this case zero .]}
+		var sentence []string
 
 		for j, sen := range strings.Split(para, ",") {
 			//read sentence
-			// fmt.Println("Sen ", j+1, ": ", sen)
 			// e.g. Sen  1 :  3 The Rock is destined to be the 21st Century 's new `` Conan '' and that he 's going to make a splash even greater than Arnold Schwarzenegger
 			// e.g. Sen  2 :   Jean-Claud Van Damme or Steven Segal .
 			//finish sentences construction
-			// trainingSentences[i][j+1] = sen
-			currentSentence = append(currentSentence, sen)
+			sentence = append(sentence, sen)
 
-			for _, word := range strings.FieldsFunc(sen, notALetter) {
+			for _, wordname := range strings.FieldsFunc(sen, notALetter) {
 				//read word
 				//finish word_map construction
 				//convert word to lower case
-				word = strings.ToLower(word)
-				words_map[word] = append(words_map[word], [][2]int{{i, j + 1}}...)
+				wordname = strings.ToLower(wordname)
+				word := words_map[wordname]
+
+				word.Appearance = append(word.Appearance, [][2]int{{i, j}}...)
+
+				word.WordRankOnEmergeTimes[class]++
+
+				words_map[wordname] = word
 			}
 		}
-		trainingSentences = append(trainingSentences, currentSentence)
+		paragraph.Sentences = sentence
+		trainingParagraph_array = append(trainingParagraph_array, paragraph)
 	}
 
+	// update WordRankOnEmergeRatio
+	for name, word := range words_map {
+		times := 0.0
+		for _, v := range word.WordRankOnEmergeTimes {
+			times += v
+		}
+		for i, _ := range word.WordRankOnEmergeRatio {
+			word.WordRankOnEmergeRatio[i] = word.WordRankOnEmergeTimes[i] / times
+		}
+		words_map[name] = word
+	}
+
+	// z := 0
 	// for key, value := range words_map {
-	// 	fmt.Printf("%s : %d\n", key, value)
+	// 	fmt.Printf("%s : %.9f : %.9f \n", key, value.WordRankOnEmergeTimes, value.WordRankOnEmergeRatio)
+	// 	if z > 100 {
+	// 		break
+	// 	}
+	// 	z++
 	// }
+
+	// fmt.Println(trainingParagraph_array)
 }
